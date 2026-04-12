b2d4d1dfe5aeec92ef56f041733f9815e079ab62
Nivethith AK <nivethith-ak@users.noreply.github.com>
2026-04-12 21:20:43 +0530

    chore: auto-sync 2026-04-12 21:20:43

--- backend/routes/platformDesigners.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getApprovedDesigners } from '../controllers/platformDesignerController.js';
-
-const router = express.Router();
-
-router.get('/', getApprovedDesigners);
-
-export default router;
