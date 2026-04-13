17b99f7e5c2ec95cf0d013c7a8ba525ed628c414
Nivethith AK <nivethith-ak@users.noreply.github.com>
2026-04-13 13:52:55 +0530

    chore: auto-sync 2026-04-13 13:52:55

--- backend/config/adminSeed.js
+++ backend/config/adminSeed.js
@@ -1,13 +1,10 @@
 import mongoose from 'mongoose';
 import User from '../models/User.js';
 import Admin from '../models/Admin.js';
-import Company from '../models/Company.js';
 
 const DEFAULT_ADMIN_EMAIL = 'test@gmail.com';
 const DEFAULT_ADMIN_PASSWORD = '123456';
 const LEGACY_ADMIN_EMAIL = 'admin@gmail.com';
-const DEFAULT_COMPANY_EMAIL = 'testing@gmail.com';
-const DEFAULT_COMPANY_PASSWORD = '123456';
 
 export const ensureAdminUser = async () => {
   if (mongoose.connection.readyState !== 1) {
@@ -70,66 +67,6 @@ export const ensureAdminUser = async () => {
       }
     }
 
-    let companyUser = await User.findOne({ email: DEFAULT_COMPANY_EMAIL }).select('+password');
-
-    if (!companyUser) {
-      companyUser = await Company.create({
-        name: 'Testing Company',
-        email: DEFAULT_COMPANY_EMAIL,
-        password: DEFAULT_COMPANY_PASSWORD,
-        role: 'company',
-        isApproved: true,
-        isVerified: true,
-        companyName: 'Testing Company',
-        industry: 'Fashion',
-        contactPerson: 'Testing Admin',
-        phone: 'N/A',
-        address: 'N/A',
-      });
-
-      console.log('Default testing company created: testing@gmail.com');
-    } else if (companyUser.role !== 'company') {
-      await User.deleteOne({ _id: companyUser._id });
-
-      companyUser = await Company.create({
-        name: 'Testing Company',
-        email: DEFAULT_COMPANY_EMAIL,
-        password: DEFAULT_COMPANY_PASSWORD,
-        role: 'company',
-        isApproved: true,
-        isVerified: true,
-        companyName: 'Testing Company',
-        industry: 'Fashion',
-        contactPerson: 'Testing Admin',
-        phone: 'N/A',
-        address: 'N/A',
-      });
-
-      console.log('Existing testing@gmail.com account replaced with company account');
-    } else {
-      let shouldSaveCompany = false;
-
-      if (!companyUser.isApproved) {
-        companyUser.isApproved = true;
-        shouldSaveCompany = true;
-      }
-
-      if (!companyUser.isVerified) {
-        companyUser.isVerified = true;
-        shouldSaveCompany = true;
-      }
-
-      const companyPasswordMatches = await companyUser.matchPassword(DEFAULT_COMPANY_PASSWORD);
-      if (!companyPasswordMatches) {
-        companyUser.password = DEFAULT_COMPANY_PASSWORD;
-        shouldSaveCompany = true;
-      }
-
-      if (shouldSaveCompany) {
-        await companyUser.save();
-        console.log('Default testing company credentials synced for testing@gmail.com');
-      }
-    }
   } catch (error) {
     console.warn(`Admin seed warning: ${error.message}`);
   }
